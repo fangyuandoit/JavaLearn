ca70cde2393194819f72599b9ed39be63bc0aa7f
fangyuandoit <676176407@qq.com>
2019-07-30 22:01:03 +0800

    netty

---
+++ src/main/java/netty_跟闪电侠学netty/NettyClient.java
@@ -0,0 +1,39 @@
+package netty_跟闪电侠学netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+/**
+ * @Author: fang
+ * @Date: 2019/7/30 21:59
+ * @Description:
+ */
+public class NettyClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        while (true) {
+            channel.writeAndFlush(new Date() + ": hello world!");
+            Thread.sleep(2000);
+        }
+    }
+}
